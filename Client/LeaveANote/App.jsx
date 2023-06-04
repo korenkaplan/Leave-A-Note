3fb2b8d4eef94ca81130e4ca97cc8177559a76a4
koren_kaplan <korenkaplan96@gmail.com>
2023-06-04 17:34:53 +0300

    Adding NativeBase

---
+++ Client/LeaveANote/App.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NativeBaseProvider } from 'native-base'
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+    <View>
+
+    </View>
+    </NativeBaseProvider>
+ 
+  )
+}
