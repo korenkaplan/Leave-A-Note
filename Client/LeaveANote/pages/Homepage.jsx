45fc1b48bb72036e6704124cd5649f34e05f7419
koren_kaplan <korenkaplan96@gmail.com>
2023-06-05 13:34:43 +0300

    Added Svg libraries

--- Client/LeaveANote/pages/Homepage.jsx
+++ Client/LeaveANote/pages/Homepage.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-
+import Logo from "../assets/note-taking.svg";
 export default function Homepage({ navigation }) {
   return (
     <View>
       <Text>Homepage</Text>
-
+      <Logo width={120} height={40} />
     </View>
   )
 }
