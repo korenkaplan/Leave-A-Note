01d4620163b8cb45dd7ee629965bb653efab756f
koren_kaplan <korenkaplan96@gmail.com>
2023-06-07 17:33:21 +0300

    Formik in Edit Password

--- Client/LeaveANote/pages/profilePages/AccidentsHistory.jsx
+++ Client/LeaveANote/pages/profilePages/AccidentsHistory.jsx
@@ -54,6 +54,7 @@ export default function AccidentsHistory() {
     return (
       <ListItem.Swipeable
         bottomDivider
+        key={item.key}
         style={{backgroundColor: 'gray'}}
         leftContent={reset => (
           <Button
--- Client/LeaveANote/pages/profilePages/EditPassword.jsx
+++ Client/LeaveANote/pages/profilePages/EditPassword.jsx
@@ -1,42 +1,82 @@
 import { View, Text,StyleSheet} from 'react-native'
 import React,{useState} from 'react'
 import { Input, Icon,Button } from '@rneui/themed';
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 export default function EditPassword() {
   const [oldPasswordSecure, setOldPasswordSecure] = useState(true)
   const [newPasswordSecure, setNewPasswordSecure] = useState(true)
   const [repeatPasswordSecure, setRepeatPasswordSecure] = useState(true)
+  const [isValid, setIsValid] = useState(false)
+ 
 
-  const  handleSubmit= () =>{
-    
-  }
-  const validateInputs =() =>{
-    
-  }
+  /**
+  The validation schema for Formik using yup
+ */
+  const validationSchema = Yup.object().shape({
+    currentPassword: Yup.string().required('Current password is required'),
+    newPassword: Yup.string()
+      .required('New password is required')
+      .min(8, 'New password must be at least 8 characters long'),
+    repeatPassword: Yup.string()
+      .required('Repeat password is required')
+      .oneOf([Yup.ref('newPassword')], 'Passwords must match')
+  });
+
+    /**
+  Handle the sbumit check if the password matches the current password in the database
+ */
+  const handleFormSubmit = (values, { resetForm }) => {
+    //TODO: validate password with database
+      // Handle form submission
+      console.log(values);
+
+      // Clear the form inputs
+      resetForm();
+  };
   return (
     <View style={styles.container}>
-      <View>
-      <Text>Currant Password:</Text>
-    <Input placeholder="Currant Password" secureTextEntry={oldPasswordSecure}
-    leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
-    rightIcon={{type:'ionicon', name : oldPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setOldPasswordSecure(!oldPasswordSecure)}}
-    />
-      </View>
-      <View>
-      <Text>New Password:</Text>
-    <Input placeholder="New Password" secureTextEntry={newPasswordSecure}
-    leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
-    rightIcon={{type:'ionicon', name : newPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setNewPasswordSecure(!newPasswordSecure)}}
-    />
-      </View>
-      <View>
-      <Text>Repeat Password:</Text>
-    <Input placeholder="Repeat Password" secureTextEntry={repeatPasswordSecure}
-    leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
-    rightIcon={{type:'ionicon', name : repeatPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setRepeatPasswordSecure(!repeatPasswordSecure)}}
-    />
-      </View>
-      <Button title='save'/>
+       <Formik
+      initialValues={{ currentPassword: '', newPassword: '', repeatPassword: '' }}
+      validationSchema={validationSchema}
+      onSubmit={handleFormSubmit}
+    >
+      {({ handleChange, handleSubmit, values, errors,resetForm }) => (
+        <View>
+          <Input
+            onChangeText={handleChange('currentPassword')}
+            value={values.currentPassword}
+            secureTextEntry={oldPasswordSecure}
+            leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
+           rightIcon={{type:'ionicon', name : oldPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setOldPasswordSecure(!oldPasswordSecure)}}
+            placeholder="Enter current password"
+          />
+          {errors.currentPassword && <Text style={styles.error}>{errors.currentPassword}</Text>}
+
+          <Input
+            onChangeText={handleChange('newPassword')}
+            value={values.newPassword}
+            secureTextEntry={newPasswordSecure}
+            leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
+            rightIcon={{type:'ionicon', name : newPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setNewPasswordSecure(!newPasswordSecure)}}
+            placeholder="Enter new password"
+          />
+          {errors.newPassword && <Text style={styles.error}>{errors.newPassword}</Text>}
+
+          <Input
+            onChangeText={handleChange('repeatPassword')}
+            value={values.repeatPassword}
+            secureTextEntry={repeatPasswordSecure}
+            leftIcon={{type:'ionicon', name: 'lock-closed-outline'}}
+            rightIcon={{type:'ionicon', name : repeatPasswordSecure?'eye-off-outline':'eye-outline',  onPress: () => setRepeatPasswordSecure(!repeatPasswordSecure)}}
+            placeholder="Repeat new password"
+          />
+          {errors.repeatPassword && <Text style={styles.error} >{errors.repeatPassword}</Text>}
+
+          <Button  title="Submit" onPress={handleSubmit} />
+        </View>
+      )}
+    </Formik>
     </View>
   )
 }
@@ -45,5 +85,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     padding:20,
+  },
+  error:{
+    color:'red'
   }
 });
+
