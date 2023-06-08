8832b9f9d6f48bcfe01e5add739aad813e035e0b
koren_kaplan <korenkaplan96@gmail.com>
2023-06-08 19:33:42 +0300

    inbox component start

--- Client/LeaveANote/pages/Inbox.jsx
+++ Client/LeaveANote/pages/Inbox.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CameraComp from '../Components/CameraComp'
+import {View, Text,StyleSheet} from 'react-native';
+import React from 'react';
+import { Divider } from '@rneui/base';
 export default function Inbox() {
   return (
-    <View style={{flex:1}}>
-      <CameraComp/>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Inbox</Text>
+      </View>
+      <Divider color='black'/>
     </View>
-  )
+  );
 }
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  header:{
+    width: '100%',
+    height: '10%',
+    backgroundColor:'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText:{
+    color:'white',
+    fontSize:25,
+  }
+});
