5035f3bed88fb5ec28edad62db40ec7e77499ebb
koren_kaplan <korenkaplan96@gmail.com>
2023-06-14 23:22:11 +0300

    splash screen update

--- Client/LeaveANote/pages/SplashScreen.tsx
+++ Client/LeaveANote/pages/SplashScreen.tsx
@@ -1,53 +1,68 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect, useState,useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+import { View,  StyleSheet, Animated } from 'react-native';
 import LottieView from 'lottie-react-native';
 import animationData from '../assets/lottie.Animation/safteycar.json';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { MainContext } from '../context/ContextProvider';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { Text } from '@rneui/base';
 
 interface Props {
   navigation: StackNavigationProp<Record<string, object>, string>;
 }
 
 const SplashScreen: React.FC<Props> = ({ navigation }) => {
-  const [isConnected, setIsConnected] = useState<boolean>(false);
-const {setAuthenticated,authenticated} = useContext(MainContext);
+  const [fadeAnimation] = useState(new Animated.Value(0));
 
+  useEffect(() => {
+    const splashTimeout = setTimeout(() => {
+      Animated.timing(fadeAnimation, {
+        toValue: 1,
+        duration:2000,
+        useNativeDriver: true,
+      }).start();
+    }, 2000);
 
-    useEffect(() => {
-        // Perform any necessary initialization or background tasks here
-        // Simulate a delay for the splash screen (e.g., 3 seconds)
-        const splashTimeout = setTimeout(() => {
-        // check if user is connected in async storage
-        },3000);
+    return () => {
+      clearTimeout(splashTimeout);
+    };
+  }, [fadeAnimation]);
 
-        return () => {
-          // Clean up any resources or timers if necessary
-          clearTimeout(splashTimeout);
-        };
-      }, []);
   return (
     <View style={styles.container}>
-           <LottieView
-          source={animationData}
-          autoPlay
-          loop
-        />
-  </View>
+        <Animated.View style={[styles.textContainer, { opacity: fadeAnimation }]}>
+        <Text h1 style={styles.text}>Leave a Note</Text>
+      </Animated.View>
+      <View style={styles.animationContainer}>
+        <LottieView source={animationData} autoPlay loop style={styles.animation} />
+      </View>
+    </View>
   );
 };
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 24,
-      fontWeight: 'bold',
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  animationContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animation: {
+    width: '80%',
+  },
+  textContainer: {
+    marginTop: 200,
+    position: 'relative',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+
 
 export default SplashScreen;
